05bbf0263df27b01df2297d9cd5375326ccddb21
Alex Birmingham <alexx1523@gmail.com>
2021-05-01 21:25:54 -0700

    prettier

--- drb_server.rb
+++ drb_server.rb
@@ -30,25 +30,24 @@ class ActionQueue
   def init_queue(char)
     if !@actions[char]
       print "Initializing queue for #{char}\n" if @debug
-      @actions[char] = {
-        "count" => 0,
-        "queue" => [],
-      }
+      @actions[char] = { 'count' => 0, 'queue' => [] }
     end
   end
-  def queue_action(char, cmd, args=[])
+  def queue_action(char, cmd, args = [])
     init_queue char
     id = @actions[char]['count']
     print "queue_action #{char}/#{cmd}\n" if @debug
 
-    @actions[char]['queue'].push({
-      "id" => id,
-      "char" => char,
-      "cmd" => cmd,
-      "args" => args,
-      "done" => false,
-      "error" => nil,
-    })
+    @actions[char]['queue'].push(
+      {
+        'id' => id,
+        'char' => char,
+        'cmd' => cmd,
+        'args' => args,
+        'done' => false,
+        'error' => nil
+      }
+    )
     @actions[char]['count'] = @actions[char]['count'] + 1
 
     return id
@@ -63,16 +62,18 @@ class ActionQueue
     init_queue char
     @actions[char]['queue'].find { |x| x['id'] == id }
   end
-  def mark_action_done(char, id, error=nil)
+  def mark_action_done(char, id, error = nil)
     print "mark_action_done #{char}/#{id}\n" if @debug
     init_queue char
     index = @actions[char]['queue'].find_index { |x| x['id'] == id }
-    @actions[char]['queue'][index] = @actions[char]['queue'][index].merge({'done' => true, "error" => error})
+    @actions[char]['queue'][index] =
+      @actions[char]['queue'][index].merge({ 'done' => true, 'error' => error })
   end
   def clear_action(char, id)
     print "clear_action #{char}/#{id}\n" if @debug
     init_queue char
-    @actions[char]['queue'] = @actions[char]['queue'].reject { |x| x['id'] == id }
+    @actions[char]['queue'] =
+      @actions[char]['queue'].reject { |x| x['id'] == id }
   end
   def clear_all_actions()
     print "clear_all_actions\n" if @debug
@@ -81,10 +82,7 @@ class ActionQueue
 end
 
 #create shared object to be hosted
-shared = {
-  'bandits' => Bandits.new,
-  'actions' => ActionQueue.new
-}
+shared = { 'bandits' => Bandits.new, 'actions' => ActionQueue.new }
 
 DRb.start_service('druby://localhost:9999', shared)
 DRb.thread.join
